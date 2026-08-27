8e4fe964e5d8ef21580f58773975bbf9983d9358
Keksuccino <Keksuccino@users.noreply.github.com>
2026-08-27 10:54:44 +0200

    Bump Rinku version to 3.0.4

--- common/src/main/java/de/keksuccino/rinku/Rinku.java
+++ common/src/main/java/de/keksuccino/rinku/Rinku.java
@@ -18,7 +18,7 @@ public final class Rinku {
 
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final String MOD_ID = "rinku";
-    public static final String VERSION = "3.0.2";
+    public static final String VERSION = "3.0.4";
 
     private static final RinkuInitializationController INITIALIZATION_CONTROLLER = new RinkuInitializationController();
     private static final HashMap<CefCursorType, Long> CEF_TO_GLFW_CURSORS = new HashMap<>();
